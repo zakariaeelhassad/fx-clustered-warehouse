4ed52a99ce77a98d5f7956621654402077892b92
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-11-11 14:39:20 +0100

    Implement DealService with create method, including duplicate ID check and logging

---
+++ src/main/java/com/progressoft/fxclusteredwarehouse/services/impl/DealService.java
@@ -0,0 +1,37 @@
+package com.progressoft.fxclusteredwarehouse.services.impl;
+
+import com.progressoft.fxclusteredwarehouse.services.IDealService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import com.progressoft.fxclusteredwarehouse.exception.DealIdException;
+import com.progressoft.fxclusteredwarehouse.mapper.DealMapper;
+import com.progressoft.fxclusteredwarehouse.models.dto.DealRequestDto;
+import com.progressoft.fxclusteredwarehouse.models.dto.DealResponseDto;
+import com.progressoft.fxclusteredwarehouse.models.entitis.Deal;
+import com.progressoft.fxclusteredwarehouse.repository.DealRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DealService implements IDealService {
+
+    private final DealRepository repository;
+    private final DealMapper mapper;
+
+    @Override
+    public DealResponseDto created(DealRequestDto dto){
+        log.info("Attempting to save deal with ID: {}", dto.id());
+
+        if(repository.existsById(dto.id())) {
+            log.warn("Duplicate deal ID detected: {}. Operation aborted.", dto.id());
+            throw new DealIdException("Deal id already exists");
+        }
+
+        Deal savedDeal = repository.save(mapper.toEntity(dto));
+        log.info("Deal saved successfully with ID: {}", savedDeal.getId());
+
+        return mapper.toResponseEntity(savedDeal);
+    }
+
+}
