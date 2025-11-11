de916426b8f95bb068a7ba52cfc47ac398e9e019
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-11-11 14:43:51 +0100

    Add FxClusteredWarehouseApplication as the main Spring Boot entry point

---
+++ src/main/java/com/progressoft/fxclusteredwarehouse/FxClusteredWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.progressoft.fxclusteredwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxClusteredWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FxClusteredWarehouseApplication.class, args);
+    }
+
+}
