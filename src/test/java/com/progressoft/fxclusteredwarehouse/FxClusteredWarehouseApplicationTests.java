dec50d9aff85efa9cdfe5088e7812965c60731f5
Zakariae El Hassad <zakariaelhassad031@gmail.com>
2025-11-11 14:58:08 +0100

    Add FxClusteredWarehouseApplicationTests to verify Spring Boot context loads

---
+++ src/test/java/com/progressoft/fxclusteredwarehouse/FxClusteredWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progressoft.fxclusteredwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxClusteredWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
